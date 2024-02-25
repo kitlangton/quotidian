52850a11eddd7485854f49e4a66aeca9c55e7a89
Kit Langton <kit.langton@gmail.com>
2024-02-25 11:31:04 -0500

    update

--- examples/shared/src/main/scala/quotidian/examples/lens/LensDemo.scala
+++ examples/shared/src/main/scala/quotidian/examples/lens/LensDemo.scala
@@ -1,22 +1,21 @@
 package quotidian.examples.lens
 
 import quotidian.examples.lens.Person.age
-import scala.annotation.experimental
 
-case class Person(name: String, age: Int):
-  def randomNumber = 42
+case class Person(name: String, age: Int)
 
 object Person:
   val lenses = LensMacros.makeLenses[Person]
   val age    = lenses.age
   val name   = lenses.name
 
-@experimental
 @main
 def example(): Unit =
   val person = Person("Alice", 42)
-  val name   = Person.name.get(person)
-  val aged   = Person.age.set(person, 43)
   println(s"Person: $person")
+
+  val name = Person.name.get(person)
   println(s"Name: $name")
+
+  val aged = Person.age.modify(person)(_ + 100)
   println(s"Aged: $aged")
